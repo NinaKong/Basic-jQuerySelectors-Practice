6d28c600649a997a0364b650a668f86467232639
NinaSleephead <kx1214@gmail.com>
2016-07-22 22:39:54 -0400

    Selectors-jQuery
    Practice for basic jQuery selectors.

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*Nina Kong*/
+$(document).ready(function(){
+	var windowHeight = $( window ).height();
+	$("#Content").height(windowHeight);
+
+    $("#Content").css({"color": "#fff", "padding": "5em"}); 
+
+    $(".fa-heart").css("color", "#de8396");
+
+    $("span").css("font-size", "2.5em");
+
+    $("span.fa-heart").css("padding-right", "0.5em");
+
+});
